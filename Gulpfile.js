83f1137e673637a47f9901947db27cb2c535bedd
Leny <info@flatland.be>
2014-02-28 19:41:37 +0100

    Add some testing gulp tasks

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,10 +1,35 @@
 var gulp = require( "gulp" ),
     jshint = require( "gulp-jshint" ),
-    coffee = require( "gulp-coffee" );
+    coffee = require( "gulp-coffee" ),
+    supervisor = require( "./index.js" );
 
 gulp.task( "compile", function () {
     gulp.src( "./index.coffee" )
         .pipe( coffee( { bare: true } ) )
         .pipe( jshint() )
         .pipe( gulp.dest( "./" ) );
+} );
+
+gulp.task( "supervisor-simple", function() {
+    supervisor( {
+        script: "test/fixture/server.js"
+    } );
+} );
+
+gulp.task( "supervisor-all", function() {
+    supervisor( {
+        script: "test/fixture/server.js",
+        args: [],
+        watch: [ "test" ],
+        ignore: [ "tasks" ],
+        pollInterval: 500,
+        extensions: [ "js" ],
+        exec: "node",
+        debug: true,
+        debugBrk: false,
+        harmony: true,
+        noRestartOn: false,
+        forceWatch: true,
+        quiet: false
+    } );
 } );
